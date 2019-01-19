0c1e50ce32415e61db23bc06f67180420a744a09
knight134 <knight134@163.com>
2019-01-19 23:18:38 +0800

    Update turtle21.py

--- turtle21.py
+++ turtle21.py
@@ -4,6 +4,7 @@ import turtle
 ninjia=turtle.Turtle()
 
 ninjia.speed(10)
+# 每次刷新的间隔时间
 
 for i in range(180):
     ninjia.forward(100)
@@ -16,7 +17,7 @@ for i in range(180):
     ninjia.penup()
     ninjia.setposition(0,0)
     ninjia.pendown()
-
+    # 一系列底层绘图函数
     ninjia.right(2)
 
 turtle.done()
