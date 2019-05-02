d850f6d2a082d8a069178ae36997cfa684541215
knight134 <knight134@126.com>
2019-05-02 21:47:22 +0800

    将判断一年中的第几天程序使用list列表实现

--- whichDayOftheYear.py
+++ whichDayOftheYear.py
@@ -16,7 +16,8 @@ import datetime
 
 class WhichDay():
     def __init__(self):
-        self.days_in_month_tup=(31,28,31,30,31,30,31,31,30,31,30,31)
+        #self.days_in_month_tup=(31,28,31,30,31,30,31,31,30,31,30,31)
+        self.days_in_month_tup = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     def set_input(self):
         input_date_str=input("请输入如期（yyyy/mm/dd）:")
         self.input_date=datetime.datetime.strptime(input_date_str,"%Y/%m/%d")
@@ -26,13 +27,28 @@ class WhichDay():
         self.month=self.input_date.month
         self.day=self.input_date.day
     def days(self):
+        if(WhichDay.is_leap_year(self.year)):
+            self.days_in_month_tup[1]=29
         sum_days=sum(self.days_in_month_tup[:self.month-1])+self.day
         # 如果是闰年需要额外的处理
+        '''
         if(self.year%400==0 or(self.year%4==0 and self.year%100!=0)):
             if(self.month>2):
                 sum_days=sum_days+1
+        '''
         # 输出日期
         print("一年中的第"+str(sum_days)+"天")
+    @staticmethod
+    def is_leap_year(year):
+        '''
+            判断一个年份是否是闰年
+        '''
+        is_leap=False
+        if(year%400==0 or(year%4==0 and year%100!=0)):
+            is_leap=True
+        return is_leap
+
+
 if(__name__=="__main__"):
     w=WhichDay()
     w.set_input()
