9e8778d30babb65da94af4a7dc5c4a8e5307605f
knight134 <knight134@126.com>
2019-05-02 21:30:03 +0800

    最后一次完成存钱挑战的代码

--- saveMoney_challenge.py
+++ saveMoney_challenge.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 # -*-coding:utf8 -*-
-
+import datetime
 '''
 52周存钱挑战
+输入一个日期，判断是第几周，然后输出该周的存款金额
 '''
 import math
 def main():
@@ -17,6 +18,7 @@ def main():
 
     saving=0          # 账户累计钱数
     money_list=[]
+    saving_list=[]     # 每周累计存钱数
     # while 循环
     while(i<=total_week):
         #saving=saving+money_per_week
@@ -25,11 +27,19 @@ def main():
         # i=i+1
         money_list.append(money_per_week)
         saving=math.fsum(money_list)
+        saving_list.append(saving)
+
         # 输出信息
         print("第{}周，存入{}元，账户累计{}元".format(i,money_per_week,saving))
         # money_list.append(money_per_week)
 
         money_per_week = money_per_week + increase_money
         i = i + 1
+
+    ###################
+    date=input("请输入日期：(yy/m/d)")
+    week=datetime.datetime.strptime(date,"%Y/%m/%d").isocalendar()[1]
+    print("该日期周数："+str(week))
+    print("第"+str(week)+"周，存入"+str(money_list[week-1])+"元，账户累计"+str(saving_list[week-1])+"元")
 if(__name__=="__main__"):
     main()
